e3dbc070461f09bd2fcc5a056bbb3861bb26dc89
Victor Cinaglia <victorcinaglia@gmail.com>
2015-03-23 00:13:56 -0400

    Fixed provisioning typo

--- provision.sh
+++ provision.sh
@@ -1,5 +1,5 @@
 export OS=`uname -s | sed -e 's/  */-/g;y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/'`
 
-for t in $HOME/dotfiles/provision/$OS/tasks/*; do
+for t in $HOME/dotfiles/provision/$OS/*; do
     sh $t
 done
