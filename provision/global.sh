b0b32f859f755ced9b389d9c285e14679f4dc9d6
Victor Cinaglia <victorcinaglia@gmail.com>
2017-02-12 14:43:09 -0500

    npm-global: Fix empty check

--- provision/global.sh
+++ provision/global.sh
@@ -8,4 +8,4 @@ modules=$(printf '%s","' "${modules[@]}" | rev | cut -c 4- | rev)
 not_installed=$(npm list -g --json | jq -r ".dependencies | [\"${modules}\"] - keys | .[]")
 
 # Install all missing modules
-test $not_installed && npm install -g ${not_installed[*]}
+test "$not_installed" && npm install -g ${not_installed[*]}
