fd1ea8f8ddfb852ca2c80266da88b5ac1e3bd8f5
Victor Cinaglia <victorcinaglia@gmail.com>
2017-05-13 12:31:21 -0400

    hammerspoon: Tweak apply to take individual arguments
    Additionally, add `compose` function which might be useful in the
    future.

--- files/.hammerspoon/utils.lua
+++ files/.hammerspoon/utils.lua
@@ -4,15 +4,30 @@
 
 local module = {}
 
+function istable(t)
+  return type(t) == 'table'
+end
+
+function tablify(v)
+  return istable(v) and v or {v}
+end
+
+-- Compose
+function module.compose(fn1, fn2)
+  return function(...)
+    return fn1(fn2(...))
+  end
+end
+
 -- Make-shift partial application
-module.apply = function(f, args)
+function module.apply(fn, args)
   return function()
-    return f(table.unpack(args))
+    return fn(module.compose(table.unpack, tablify)(args))
   end
 end
 
 -- Add a delay to a given function
-module.delay = function(fn, delay)
+function module.delay(fn, delay)
   return module.apply(hs.timer.doAfter, {delay, fn})
 end
 
